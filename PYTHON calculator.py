d1cdd8ccc93c2fec9239423c30542840c38f2bcb
Ittussarom Retals Mail Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2020-10-10 15:05:58 -0400

    Much better code
    - Looks better
    - Works better
    - Now shows calculation choices instead of in docstring
    - Grammar

--- PYTHON calculator.py
+++ PYTHON calculator.py
@@ -1,5 +1,4 @@
-print (
-
+print(
 '''
 ╔═══╗──╔╗──────╔╗───╔╗
 ║╔═╗║──║║──────║║──╔╝╚╗
@@ -9,59 +8,50 @@ print (
 ╚═══╩╝╚╩═╩══╩══╩═╩╝╚╩═╩══╩╝
 ''') 
 
-#first read this------------\
-#                           |
-#                           |
-#                           /          
-#                          √
-'''
-   print("Your_Calculation")
-   print("for_addition : type 'add'")
-   print("for_multiplication : 'multiply'")
-   print("for_division : 'divide'")
-   print("for_subtration :subtract")
-   print("for_square_root: root")
-   print("for_cancle_processing :'clear'")
-'''  
+print("Your_Calculation")
+print("for_addition : type 'add'")
+print("for_multiplication : 'multiply'")
+print("for_division : 'divide'")
+print("for_subtration :subtract")
+print("for_square_root: root")
+print("for_cancle_processing :'clear'")
+
 while True:
-   user_input = input()
+   user_input = input("Enter command: ")
    
    if user_input == "clear":
      break 
     
-   elif user_input== 'add':
-    num1 = float(input("Enter a number: "))
-    num2 = float(input("Enter b number: "))
-    result= (num1 +num2)
+   elif user_input == 'add':
+    num1 = float(input("Enter number a: "))
+    num2 = float(input("Enter number b: "))
+    result = num1 + num2
     print(result)
       
-   elif user_input=='multiply':
-     num1 =float(input(""))
-     num2 =float(input(""))
-     result=(num1*num2)
+   elif user_input == 'multiply':
+     num1 = float(input("Enter number a: "))
+     num2 = float(input("Enter number b: "))
+     result = num1 * num2
      print(result)
-   elif user_input=='divide':
-     num1 =float(input(""))
-     num2 =float(input(""))
-     result =(num1 //num2)
-     print(result)
-
-   elif user_input=='subtract':
-     num1 =float(input(""))
-     num2 =float(input(""))
-     result =(num1 - num2)
+   elif user_input == 'divide':
+     num1 =float(input("Enter number a: "))
+     num2 =float(input("Enter number b:"))
+     result = num1 / num2
      print(result)
 
+   elif user_input == 'subtract':
+     num1 = float(input("Enter number a: "))
+     num2 = float(input("Enter number b: "))
+     result = num1 - num2
+     print(result)
 
-   elif user_input=='root':
-     num1 =float(input(""))
-     num2 =float(input(""))
-     result1 = (num1**1/2) 
-     result2=  (num2**1/2) 
-     print(result1)
-     print(result2 )
+
+   elif user_input == 'root':
+     num1 = float(input("Enter number: "))
+     result = num1 ** 0.5
+     print(result)
      
-   elif user_input=='percentage':
-     num1 =float(input(""))
-     result1 = (num1/100) 
+   elif user_input == 'percentage':
+     num1 = float(input("Enter number: "))
+     result = num1 / 100)
      print(result1)
